31793e5f657e87fdd602061d5b0aecb3c2a7ade0
prsn-ts <turbo6040@gmail.com>
2020-09-29 12:55:14 +0900

    클래스 기반의 컴포넌트가 활성화될 때(componentDidMount 함수 관련) Cross Origin(기존 서버가 아닌 다른 서버로의 ajax 요청) 방식으로 사용하는 예제(App10_Ajax3.js) 추가함.

---
+++ src/App10_Ajax3.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import './css/bootstrap.css';
+
+class App10_Ajax2 extends Component{
+    //상태값 
+    state={
+        cafeList:[]
+    };
+
+    //부모 컴포넌트를 재정의(오버라이드)하는 함수.
+    //해당 컴포넌트가 활성화 되었을 때 호출되는 메소드.(화살표 함수도 가능하다.)
+    //컴포넌트가 활성화될 때(이 페이지가 로딩될 때) ajax 요청을 통해 데이터를 받아와서 화면에 출력되도록 한다.
+    componentDidMount(){
+        console.log("App10_Ajax3 컴포넌트가 마운드 되었습니다.");
+        fetch("http://localhost:8888/spring05/cafe/ajax_list.do")
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            const result=data.list.map((item)=>{
+                return (
+                    <tr key={item.num}>
+                        <td>{item.num}</td>
+                        <td>{item.title}</td>
+                        <td>{item.writer}</td>
+                    </tr>
+                );
+            });
+            //setState() 함수를 이용해서 상태값을 바꿔준다. 
+            this.setState({cafeList:result});
+        })
+        .catch(err=>{
+            console.log(err);
+        });
+    }
+
+    //요청하기 버튼을 눌렀을때 호출되는 함수 
+    request=()=>{
+        
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <h1>ajax 요청 테스트</h1>
+                <button onClick={this.request}>요청하기</button>
+                <table className="table">
+                    <thead className="thead-dark">
+                        <tr>
+                            <th>글번호</th>
+                            <th>제목</th>
+                            <th>작성자</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.cafeList}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+export default App10_Ajax2;
