e7f375b4581f1ff7520d14f89e34952dfe2ebab0
prsn-ts <turbo6040@gmail.com>
2020-09-29 11:02:22 +0900

    라우터를 적용한 컴포넌트(App09_SinglePage) 의 하위 컴포넌트에도 라우터의 기능을 사용하기위한 withRouter 함수 예제(App09_SinglePage.js) 추가함.

--- src/App09_SinglePage.js
+++ src/App09_SinglePage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavLink, Route } from 'react-router-dom';
+import { NavLink, Route, withRouter } from 'react-router-dom';
 //index.js 파일안에 있는 Buy, Game, Home, Study등 여러개의 컴포넌트를 한꺼번에 불러오기.(여러 컴포넌트를 로드할 때 유용)
 import { Buy, Game, Home, Study} from './pages';
 /* index.js 파일을 사용하지 않았을 때 import 방법
@@ -9,16 +9,31 @@ import Home from './pages/home';
 import Study from './pages/Study';
 */
 //리액트에서 싱글페이지 기반 웹페이지 구성을 어떻게하는지 연습하는 예제
-const App09_SinglePage=()=>{
+const App09_SinglePage=({location, match, history})=>{
 
     const activeStyle={
         color: "red",
         fontWeight:"bold"
     };
 
+    const [path, setPath]=React.useState('');
+
+    //input 요소에 입력한 값이 변했을 때 경로를 바꿔주고
+    const changed=(e)=>{
+        setPath(e.target.value);
+    };
+    //이동 버튼을 클릭했을 때 실행할 함수(해당 경로로 이동 시킨다.)
+    const move=()=>{
+        console.log(path);
+        //history 객체를 이용해서 강제 이동 시킨다.
+        history.push(path); //이 구문으로 인해 history에 쌓이면서 페이지 이동도 된다.
+    };
+
     return (
         <div>
             <h1>Router 테스트</h1>
+            <input onChange={changed} value={path} type="text" placeholder="이동할 경로 입력..." />
+            <button onClick={move}>이동</button>
             <ul>
                 <h2>다른 페이지로 이동할 때 깜박이면서 이동하는 페이지 전환.(싱글 페이지 기반 X)</h2>
                 {/* 누를 때 깜박이면서 다른 페이지로 이동한다.(싱글 페이지 X) */}
@@ -61,4 +76,14 @@ const App09_SinglePage=()=>{
     );
 };
 
-export default App09_SinglePage;
+/*
+    withRouter() 함수를 호출해서 export 해주면
+    Router에 관련된 props 가 이 컴포넌트에 전달된다.
+    *사실 App09_SinglePage 컴포넌트의 하위 컴포넌트인 Buy, Game, Study, Home
+    는 router 설정을 하지 않았지만 라우터 관련된 withRouter 함수를 이용해서
+    withRouter(App09_SinglePage) 이렇게 App09_SinglePage를 withRouter 함수의
+    인자로 넣어주면 App09_SinglePage 하위에 있는 직접적으로 라우터를 설정하지 않은
+    하위 컴포넌트인 Buy, Game, Study, Home도 props 속성을 이용해 {location, match, history} 이런 기능을
+    사용할 수 있게된다.
+*/
+export default withRouter(App09_SinglePage);
