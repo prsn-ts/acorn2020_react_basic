6b3c2548862154920218185df3c8485ee69a2209
prsn-ts <turbo6040@gmail.com>
2020-09-29 10:39:17 +0900

    NavLink 컴포넌트의 특정 링크를 눌렀을 때(활성화) 스타일 효과를 줄 수 있는 activeStyle과 활성화 상태일 때 클래스를 추가하는activeClassName 속성을 사용하는 예제(App09_SinglePage.js) 추가함.

--- src/App09_SinglePage.js
+++ src/App09_SinglePage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, Route } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
 //index.js 파일안에 있는 Buy, Game, Home, Study등 여러개의 컴포넌트를 한꺼번에 불러오기.(여러 컴포넌트를 로드할 때 유용)
 import { Buy, Game, Home, Study} from './pages';
 /* index.js 파일을 사용하지 않았을 때 import 방법
@@ -10,6 +10,12 @@ import Study from './pages/Study';
 */
 //리액트에서 싱글페이지 기반 웹페이지 구성을 어떻게하는지 연습하는 예제
 const App09_SinglePage=()=>{
+
+    const activeStyle={
+        color: "red",
+        fontWeight:"bold"
+    };
+
     return (
         <div>
             <h1>Router 테스트</h1>
@@ -19,18 +25,23 @@ const App09_SinglePage=()=>{
                 <li><a href="/">home</a></li>
                 <li><a href="/study">study</a></li>
                 <li><a href="/game">game</a></li><br />
-                <h2>리액트의 Link 속성을 이용해서 다른 페이지로 이동할 때 깜박임 없이 이동하는 페이지 전환.(싱글 페이지 기반 O)</h2>
-                {/* Link 컴포넌트를 이용해 누를 때 다른 페이지로 이동한다.(싱글 페이지 O, 한 페이지 내에서 특정 링크를 불러옴) */}
-                <li><Link to="/">home</Link></li>
-                <li><Link to="/study">study</Link></li>
-                <li><Link to="/game">game</Link></li><br />
-                <h2>리액트의 Link 속성을 이용해서 파라미터 값으로 "star" or "cart"를 넘긴다.(싱글 페이지 기반 O)</h2>
-                <li><Link to="/game/star">game</Link></li>
-                <li><Link to="/game/cart">game</Link></li><br />
-                <h2>리액트의 Link 속성을 이용해서 파라미터로 num and name을 넘긴다.(싱글 페이지 기반 O)</h2>
-                <li><Link to="/buy?num=1&name=Handphone">1번 상품 사기</Link></li>
-                <li><Link to="/buy?num=2&name=Notebook">2번 상품 사기</Link></li>
-                <li><Link to="/buy?num=3&name=Desktop">3번 상품 사기</Link></li>
+                <h2>리액트의 NavLink 속성을 이용해서 다른 페이지로 이동할 때 깜박임 없이 이동하는 페이지 전환.(싱글 페이지 기반 O)</h2>
+                {/* 
+                    NavLink, Link 컴포넌트를 이용해 누를 때 다른 페이지로 이동한다.(싱글 페이지 O, 한 페이지 내에서 특정 링크를 불러옴)
+                    NavLink 컴포넌트에는 activeStyle과 activeClassName의 속성이 있다.
+                    activeStyle은 특정 어떤 버튼이나 a 태그를 눌렀을 때(active, 활성화 상태) 스타일 효과를 주는 것이고
+                    activeClassName는 특정 어떤 버튼이나 a 태그를 눌렀을 때(active, 활성화 상태) 클래스를 추가하는 기능을 한다.
+                */}
+                <li><NavLink to="/" activeStyle={activeStyle}>home</NavLink></li>
+                <li><NavLink to="/study" activeStyle={activeStyle}>study</NavLink></li>
+                <li><NavLink to="/game" activeStyle={activeStyle}>game</NavLink></li><br />
+                <h2>리액트의 NavLink 속성을 이용해서 파라미터 값으로 "star" or "cart"를 넘긴다.(싱글 페이지 기반 O)</h2>
+                <li><NavLink to="/game/star" activeStyle={activeStyle}>game</NavLink></li>
+                <li><NavLink to="/game/cart" activeStyle={activeStyle}>game</NavLink></li><br />
+                <h2>리액트의 NavLink 속성을 이용해서 파라미터로 num and name을 넘긴다.(싱글 페이지 기반 O)</h2>
+                <li><NavLink to="/buy?num=1&name=Handphone">1번 상품 사기</NavLink></li>
+                <li><NavLink to="/buy?num=2&name=Notebook">2번 상품 사기</NavLink></li>
+                <li><NavLink to="/buy?num=3&name=Desktop">3번 상품 사기</NavLink></li>
             </ul>
             {/* 
                 exact 속성은 정확히 일치할 때만 페이지 이동이 되도록 설정하는 역할이다.
