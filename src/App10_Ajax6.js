3f48828b691d9b00382a1d43f3cf7067ff1b74b1
gura <oli999@naver.com>
2020-10-06 10:20:35 +0900

    App10_Ajax6.js 수정함

--- src/App10_Ajax6.js
+++ src/App10_Ajax6.js
@@ -4,7 +4,11 @@ import qs from 'query-string';
 
 const App10_Ajax6=()=>{
 
-    const [formData, setFormData]=useState({id:'', pwd:''});
+    const [formData, setFormData]=useState({
+        id:'', 
+        pwd:'',
+        loginId:null
+    });
 
     const changed=(e)=>{
         //이벤트가 일어난 input 요소의 name 속성의 값 ( id or pwd ) 읽어오기 
@@ -28,14 +32,41 @@ const App10_Ajax6=()=>{
         .then(res=>res.json())
         .then(data=>{
             console.log(data);
+            if(data.isSuccess){
+                setFormData({
+                    ...formData,
+                    loginId:data.id
+                });
+            }else{
+                alert("아이디 혹은 비밀번호가 틀려요!");
+            }        
         })
         .catch(err=>{
             console.log(err);
         });
 
     };
+
+    const logout=()=>{
+        //ajax 로 로그아웃 요청을 한다.
+        fetch("http://localhost:8888/spring05/users/ajax_logout.do")
+        .then(res=>res.json())
+        .then(data=>{
+            if(data.isSuccess){
+                setFormData({
+                    ...formData,
+                    loginId:null
+                });
+                alert("로그 아웃 되었습니다.");
+            }
+        });
+    };
+
     return (
         <div>
+            { formData.loginId && 
+                <p><strong>{formData.loginId}</strong>님 로그인중...</p>
+            }
             <h1>로그인 폼 입니다.</h1>
             <form onSubmit={submit}>
                 <input onChange={changed} name="id" type="text" placeholder="아이디..."/>
@@ -44,6 +75,7 @@ const App10_Ajax6=()=>{
             </form>
             <p>{JSON.stringify(formData)}</p>
             <p>{qs.stringify(formData)}</p>
+            <button onClick={logout}>로그아웃</button>
         </div>
     );
 };
